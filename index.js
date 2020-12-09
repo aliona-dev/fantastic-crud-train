7ce900ad76d0876c209c625c1a0f979b8889b298
kitsune777 <watermonkey777gmail.com>
2020-12-08 20:06:58 -0800

    added port

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const { Server } = require("mongodb");
 const app = express();
 
 //middlewares
 app.use(bodyParser.urlencoded({ extended: true }));
+Server.use(bodyParser.json());
 
-app.listen(3000 || "https://crud-tastic-demo.herokuapp.com/");
+app.listen(process.env.PORT || 3000);
+//"https://crud-tastic-demo.herokuapp.com/
 
 allStarWars = [];
 app.get("/", (req, res) => {
